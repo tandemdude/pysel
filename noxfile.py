2a636632d5f8b816b0a7b91357fabeac67a93204
tandemdude <olijdthompson@gmail.com>
2022-08-24 15:33:34 +0100

    Add nox and github actions workflow

---
+++ noxfile.py
@@ -0,0 +1,36 @@
+import os
+
+import nox
+from nox import options
+
+PATH_TO_PROJECT = os.path.join(".", "pysel")
+SCRIPT_PATHS = [
+    PATH_TO_PROJECT,
+    "noxfile.py",
+]
+
+options.sessions = ["format_fix", "mypy"]
+
+
+@nox.session()
+def format_fix(session):
+    session.install("-Ur", "dev_requirements.txt")
+    session.run("python", "-m", "black", *SCRIPT_PATHS)
+    session.run("python", "-m", "isort", *SCRIPT_PATHS)
+
+
+@nox.session()
+def format_check(session):
+    session.install("-Ur", "dev_requirements.txt")
+    session.run("python", "-m", "black", *SCRIPT_PATHS, "--check")
+
+
+@nox.session()
+def mypy(session):
+    session.install("-Ur", "dev_requirements.txt")
+    session.run("python", "-m", "mypy", "lightbulb")
+
+
+# @nox.session(reuse_venv=True)
+# def sphinx(session):
+#     session.run("python", "-m", "sphinx.cmd.build", "docs/source", "docs/build", "-b", "html")
